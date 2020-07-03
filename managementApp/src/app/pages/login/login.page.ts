0e5901e30a5453ab14b61e19f30fbeded3f72923
Aenah Ramones <aenah.ramones@gmail.com>
2020-07-03 16:05:02 -0700

    [annah] creatd login page and implemented auth service

---
+++ managementApp/src/app/pages/login/login.page.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoadingController, AlertController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private loadingCtrl: LoadingController,
+    private auth: AuthService, private alertCtrl: AlertController, 
+    private router: Router) { }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['annah@btt.com', [Validators.required, Validators.email]],
+      password: ['654321', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  async login() {
+    let loading = await this.loadingCtrl.create({
+      message: 'Loading...'
+    });
+    await loading.present();
+
+    this.auth.login(this.loginForm.value).subscribe(
+      user => {
+        loading.dismiss();
+        let role = user['role'];
+        if (role == 'PRINCIPAL' || role == 'TEACHER') {
+          // this.router.navigateByUrl('/admin/users');
+        } else if (role == 'STUDENT') {
+          // this.router.navigateByUrl('/student');
+        }
+      },
+      async err => {
+        loading.dismiss();
+
+        let alert = await this.alertCtrl.create({
+          header: 'Error',
+          message: err.error.message,
+          buttons: ['OK']
+        });
+        alert.present();
+      }
+    );
+  }
+
+}
